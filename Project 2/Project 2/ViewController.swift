dfb5e74cecef5f8c091ae16c62c43fada14e85d9
iyinraphael <iyinraphael@gmail.com>
2019-07-23 21:02:42 -0700

    added borderwith to button using CALayer and CGcolor

--- Project 2/Project 2/ViewController.swift
+++ Project 2/Project 2/ViewController.swift
@@ -14,11 +14,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
     
+    var countries = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        countries += [
+            "estonia",
+            "france",
+            "germany",
+            "ireland",
+            "italy",
+            "monaco",
+            "nigeria",
+            "poland",
+            "russia",
+            "spain",
+            "uk",
+            "us"
+        ]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+
+        askQuestioln()
     }
 
+    func askQuestioln() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
 
 }
 
