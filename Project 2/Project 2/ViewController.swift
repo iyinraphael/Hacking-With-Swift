af4372e7f4bf91e08bf7afa58d0245a142ff453a
iyinraphael <iyinraphael@gmail.com>
2019-07-24 19:25:35 -0700

    more options to game

--- Project 2/Project 2/ViewController.swift
+++ Project 2/Project 2/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var button3: UIButton!
     
     var countries = [String]()
+    var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,6 +53,10 @@ class ViewController: UIViewController {
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        correctAnswer = Int.random(in: 0...2)
+        
+        self.title = countries[correctAnswer].uppercased()
     }
 
 }
