233fe6ad81bb82611d41d1cdf9cb1128bf156e2a
iyinraphael <iyinraphael@gmail.com>
2019-11-22 13:52:49 -0800

    using autolayout for landscape viewing for project 2

---
+++ Project 6/Project 2/ViewController.swift
@@ -0,0 +1,94 @@
+//
+//  ViewController.swift
+//  Project 2
+//
+//  Created by Iyin Raphael on 7/23/19.
+//  Copyright © 2019 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var correctAnswer = 0
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareScore))
+        
+        countries += [
+            "estonia",
+            "france",
+            "germany",
+            "ireland",
+            "italy",
+            "monaco",
+            "nigeria",
+            "poland",
+            "russia",
+            "spain",
+            "uk",
+            "us"
+        ]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+
+        askQuestion(action: nil)
+    }
+
+    func askQuestion(action: UIAlertAction!) {
+        
+        countries.shuffle()
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        correctAnswer = Int.random(in: 0...2)
+        
+        self.title = countries[correctAnswer].uppercased()
+    }
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        if sender.tag == correctAnswer {
+            self.title = "Correct"
+            score += 1
+        } else {
+            self.title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: self.title, message: "Your score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true, completion: nil)
+    
+    }
+    
+    @objc func  shareScore() {
+        var score = "My score is \(self.score)."
+        if self.score > 4 {
+            score += " Great job!"
+        }
+        
+        let av = UIActivityViewController(activityItems: [score], applicationActivities: [])
+        av.popoverPresentationController?.sourceView = self.view
+        present(av, animated: true)
+    }
+    
+}
+
