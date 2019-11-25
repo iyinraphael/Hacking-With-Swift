8586e06eaf35ac244557dfd5378335c0f17568ff
iyinraphael <iyinraphael@gmail.com>
2019-11-25 10:00:21 -0800

    initial set up of project 7 touching topics in UITabar and JSON parsing

---
+++ Project 7/Project 7/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Project 7
+//
+//  Created by Iyin Raphael on 11/25/19.
+//  Copyright © 2019 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+   
+
+}
+
