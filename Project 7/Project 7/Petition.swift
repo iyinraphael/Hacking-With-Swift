83ed92e320cdb6bf42d970d7f0832c620461c624
iyinraphael <iyinraphael@gmail.com>
2019-11-26 09:45:18 -0800

    creatd petition model for Project 7

---
+++ Project 7/Project 7/Petition.swift
@@ -0,0 +1,19 @@
+//
+//  Petition.swift
+//  Project 7
+//
+//  Created by Iyin Raphael on 11/26/19.
+//  Copyright © 2019 Iyin Raphael. All rights reserved.
+//
+
+import Foundation
+
+struct Petition {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
+
+struct Petitions {
+    var results: [Petition]
+}
